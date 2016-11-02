fb202019ff1e486582bc1a8e726013050328f8c5
hardysim <hardysim@gmail.com>
2016-11-02 11:48:40 +0100

    add submodule

---
+++ submodules/j2objc_test17_bridging-header.h
@@ -0,0 +1,7 @@
+// J2ObjC requirement for Java Runtime Environment
+// Included from /J2OBJC_HOME/include
+#import "JreEmulation.h"
+
+// Swift accessible J2ObjC translated classes
+// Included from `shared/build/j2objcOutputs/src/main/objc`
+#import "MyClass.h"
